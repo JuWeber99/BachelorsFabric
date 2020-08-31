5fdd9c9cb7fa5017abc5d6a7c3f72768b22fb745
JuWeber99 <s_weberj@hwr-berlin.de>
2020-08-31 10:18:13 +0200

    prod builds

---
+++ hypersub/poc-app/build/precache-manifest.4feafededb1da9a917bb89b2b42a8c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cae6956e361fe91557233ef91b497e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecaae4700c96592e5ed",
+    "url": "/static/css/main.934b0833.chunk.css"
+  },
+  {
+    "revision": "e523decb9fe47a3c6964",
+    "url": "/static/js/2.18123ffc.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.18123ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecaae4700c96592e5ed",
+    "url": "/static/js/main.2c7493b0.chunk.js"
+  },
+  {
+    "revision": "c9762db24e17dd7a2f9f",
+    "url": "/static/js/runtime-main.887fb3cd.js"
+  },
+  {
+    "revision": "55771dbc7a4d79f57fd0684a73f60cd4",
+    "url": "/static/media/Infinity-1.1s-200px.55771dbc.gif"
+  }
+]);
